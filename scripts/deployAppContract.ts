098cd8831fefe93e509538337210a5b08ac12a0b
Timurhaip <timur2010rumit0102@gmail.com>
2026-05-29 19:37:04 +0300

    Add files via upload

---
+++ scripts/deployAppContract.ts
@@ -0,0 +1,19 @@
+import { toNano } from '@ton/core';
+import { AppContract } from '../build/AppContract/AppContract_AppContract';
+import { NetworkProvider } from '@ton/blueprint';
+
+export async function run(provider: NetworkProvider) {
+    const appContract = provider.open(await AppContract.fromInit());
+
+    await appContract.send(
+        provider.sender(),
+        {
+            value: toNano('0.05'),
+        },
+        null,
+    );
+
+    await provider.waitForDeploy(appContract.address);
+
+    console.log(`DEPLOYED: ADDRESS ${appContract.address}`)
+}
